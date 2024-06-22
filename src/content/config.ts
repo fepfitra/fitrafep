bbeb550142541054ffd0b03a1afd2556eeb186db
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2024-06-22 23:21:31 +0700

    ref: split projects and certificates

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,18 @@
 import { defineCollection, z } from 'astro:content';
 
 export const collections = {
-	work: defineCollection({
+	projects: defineCollection({
+		type: 'content',
+		schema: z.object({
+			title: z.string(),
+			description: z.string(),
+			publishDate: z.coerce.date(),
+			tags: z.array(z.string()),
+			img: z.string(),
+			img_alt: z.string().optional(),
+		}),
+	}),
+	certificates: defineCollection({
 		type: 'content',
 		schema: z.object({
 			title: z.string(),
